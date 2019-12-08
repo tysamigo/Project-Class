6dc23b3943baad7360babc6128c8b693000a7488
Tyler Ramage <tysamigo@gmail.com>
2019-12-08 16:48:23 -0500

    export questionKeys, pass questionKeys to onclick events

--- src/Troubleshooting/Questions.js
+++ src/Troubleshooting/Questions.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 
+export const QUESTION_KEY_BRAKES = "brakes";
+export const QUESTION_KEY_ENGINE = "engine";
+export const QUESTION_KEY_HEATING_AC = "heating-ac";
+export const QUESTION_KEY_STEERING_HANDLING = "steering-handling";
+export const QUESTION_KEY_TIRES_WHEELS = "tires-wheels";
+
 export class Questions extends React.Component {
     render() {
         return (
             <ListGroup className="pb-5">
-                <ListGroup.Item action onClick={this.props.chooseQuestion}>Brakes</ListGroup.Item>
-                <ListGroup.Item action onClick={this.props.chooseQuestion}>Engine</ListGroup.Item>
-                <ListGroup.Item action onClick={this.props.chooseQuestion}>Heating and A/C</ListGroup.Item>
-                <ListGroup.Item action onClick={this.props.chooseQuestion}>Steering & Handling</ListGroup.Item>
-                <ListGroup.Item action onClick={this.props.chooseQuestion}>Tires & Wheels</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.props.chooseQuestion(QUESTION_KEY_BRAKES)}>Brakes</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.props.chooseQuestion(QUESTION_KEY_ENGINE)}>Engine</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.props.chooseQuestion(QUESTION_KEY_HEATING_AC)}>Heating and A/C</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.props.chooseQuestion(QUESTION_KEY_STEERING_HANDLING)}>Steering & Handling</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.props.chooseQuestion(QUESTION_KEY_TIRES_WHEELS)}>Tires & Wheels</ListGroup.Item>
             </ListGroup>
         );
     }
