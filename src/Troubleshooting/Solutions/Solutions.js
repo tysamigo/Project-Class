f13a5f44a5deddfea3bb50a00377722b2bb18ef3
Tyler Ramage <tysamigo@gmail.com>
2019-12-08 18:30:56 -0500

    add HeatingAcSolutions to render method

--- src/Troubleshooting/Solutions/Solutions.js
+++ src/Troubleshooting/Solutions/Solutions.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { QUESTION_KEY_BRAKES, QUESTION_KEY_ENGINE } from '../Questions';
+import { QUESTION_KEY_BRAKES, QUESTION_KEY_ENGINE, QUESTION_KEY_HEATING_AC } from '../Questions';
 import BrakeSolutions from './Brake';
 import EngineSolutions from './Engine';
+import HeatingAcSolutions from './HeatingAc';
 
 export default class Solutions extends React.Component {
     render() {
@@ -11,6 +12,8 @@ export default class Solutions extends React.Component {
             solution = <BrakeSolutions solutionKey={this.props.solutionKey}/>;
         } else if (this.props.questionKey === QUESTION_KEY_ENGINE) {
             solution = <EngineSolutions solutionKey={this.props.solutionKey} />;
+        } else if (this.props.questionKey === QUESTION_KEY_HEATING_AC) {
+            solution = <HeatingAcSolutions solutionKey={this.props.solutionKey} />;
         }
 
         return solution;
