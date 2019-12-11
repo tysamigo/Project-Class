2010427809e6486e137a93795cf6bb7ae1890747
Tyler Ramage <tysamigo@gmail.com>
2019-12-11 16:04:15 -0500

    add TiresWheels module to solutions.js

--- src/Troubleshooting/Solutions/Solutions.js
+++ src/Troubleshooting/Solutions/Solutions.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { QUESTION_KEY_BRAKES, QUESTION_KEY_ENGINE, QUESTION_KEY_HEATING_AC, QUESTION_KEY_STEERING_HANDLING } from '../Questions';
+import { QUESTION_KEY_BRAKES, QUESTION_KEY_ENGINE, QUESTION_KEY_HEATING_AC, QUESTION_KEY_STEERING_HANDLING, QUESTION_KEY_TIRES_WHEELS } from '../Questions';
 import BrakeSolutions from './Brake';
 import EngineSolutions from './Engine';
 import HeatingAcSolutions from './HeatingAc';
 import SteeringHandling from './SteeringHandling';
+import TiresWheels from './TiresWheels';
 
 export default class Solutions extends React.Component {
     render() {
@@ -17,8 +18,9 @@ export default class Solutions extends React.Component {
             solution = <HeatingAcSolutions solutionKey={this.props.solutionKey} />;
         } else if (this.props.questionKey === QUESTION_KEY_STEERING_HANDLING) {
             solution = <SteeringHandling solutionKey={this.props.solutionKey} />;
+        } else if (this.props.questionKey === QUESTION_KEY_TIRES_WHEELS) {
+            solution = <TiresWheels solutionKey={this.props.solutionKey} />;
         }
-
         return solution;
     }
 }
