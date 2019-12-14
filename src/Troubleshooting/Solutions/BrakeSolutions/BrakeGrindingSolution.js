eafa9b5982f59b6e715c1d2d55cc644799ed51bc
Tyler Ramage <tysamigo@gmail.com>
2019-12-14 16:12:42 -0500

    add cards for 'replacing front brake pads' and 'replacing rear brake pads' to BrakeGrindingSolutions.js

--- src/Troubleshooting/Solutions/BrakeSolutions/BrakeGrindingSolution.js
+++ src/Troubleshooting/Solutions/BrakeSolutions/BrakeGrindingSolution.js
@@ -1,22 +1,43 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Container, Row, Col } from 'react-bootstrap';
 import brakes1 from '../../../assets/images/brakes_1.jpg';
 
 export default class BreakGrindingSolution extends React.Component {
     render() {
         return (
             <div>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={brakes1} />
-                    <Card.Body>
-                        <Card.Title>Replacing Brake Pads</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+                <Container>
+                    <Row>
+                        <Col>
+                            <p>Col 1 of 2</p>
+                            <Card style={{ width: '19rem' }}>
+                                <Card.Header as="h5">Front Brakes</Card.Header>
+                                <Card.Img variant="top" src={brakes1} />
+                                <Card.Body>
+                                    <Card.Title>Replacing Front Brake Pads</Card.Title>
+                                    <Card.Text className="mt-2">
+                                        Step by step instructions (with pics) on how to replace the Front Brake Pads.
+                                    </Card.Text>
+                                    <Button action variant="primary">Show me how</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <p>Col 2 of 2</p>
+                            <Card style={{ width: '19rem' }}>
+                                <Card.Header as="h5">Rear Brakes</Card.Header>
+                                <Card.Img variant="top" src={brakes1} />
+                                <Card.Body>
+                                    <Card.Title>Replacing Rear Brake Pads</Card.Title>
+                                    <Card.Text className="mt-2">
+                                        Step by step instructions (with pics) on how to replace the Rear Brake Pads.
+                                    </Card.Text>
+                                    <Button action variant="primary">Show me how</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
 
