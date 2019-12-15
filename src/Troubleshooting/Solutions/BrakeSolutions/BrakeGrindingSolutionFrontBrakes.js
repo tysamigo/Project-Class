4524b8ae2388d16ccdea59f4d9139ecb49372465
Tyler Ramage <tysamigo@gmail.com>
2019-12-15 14:49:13 -0500

    create new module BrakeGrindingSolutionFrontBrakes.js

---
+++ src/Troubleshooting/Solutions/BrakeSolutions/BrakeGrindingSolutionFrontBrakes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+export default class BrakeGrindingSolutionFrontBrakes extends React.Component {
+    render() {
+        return (
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <p>Hello world</p>
+                        </Col>                        
+                    </Row>
+                </Container>
+            </div>
+        );
+
+    }
+}
