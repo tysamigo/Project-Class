a264487b3a57aa8bfca3f7d18495429d47295f17
Tyler Ramage <tysamigo@gmail.com>
2019-12-15 21:35:47 -0500

    add cards to maintenance.js for oil change, transmission fluid and radiator flush

--- src/Maintenance.js
+++ src/Maintenance.js
@@ -1,14 +1,63 @@
 /* eslint-disable no-unused-expressions */
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import brakes1 from './assets/images/brakes_1.jpg';
+import brakes2 from './assets/images/brakes_2.jpg';
+import oil1 from './assets/images/oil_1.jpg';
+import radiator1 from './assets/images/radiator_1.jpg';
+import transmission1 from './assets/images/transmission_1.jpg';
 
 export default class Maintenance extends React.Component {
- 
+
     render() {
         return (
-            <Container className="heading pb-5">
-                <h1 className="mt-5">Maintenance</h1>                
-            </Container>
+            <div>
+                <Container className="pb-5">
+                    <h1 className="heading mt-5 mb-5">Maintenance</h1>
+                    <Row>
+                        <Col>
+                            <Card>
+                                <Card.Header as="h5">Oil Change</Card.Header>
+                                <Card.Img variant="top" src={oil1} />
+                                <Card.Body>
+                                    <Card.Title>Changing Oil & Filter</Card.Title>
+                                    <Card.Text className="mt-2">
+                                        Step by step instructions (with pics) on how to change the oil and filter.
+                                    </Card.Text>
+                                    <Button action variant="primary">Show me how</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                                <Card.Header as="h5">Flush Radiator</Card.Header>
+                                <Card.Img variant="top" src={radiator1} />
+                                <Card.Body>
+                                    <Card.Title>Flushing Radiator and refill</Card.Title>
+                                    <Card.Text className="mt-2">
+                                        Step by step instructions (with pics) on how to flush the radiator and Refill.
+                                    </Card.Text>
+                                    <Button action variant="primary">Show me how</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                                <Card.Header as="h5">Transmission Fluid</Card.Header>
+                                <Card.Img variant="top" src={transmission1} />
+                                <Card.Body>
+                                    <Card.Title>Replacing Transmission Fluid and Filter</Card.Title>
+                                    <Card.Text className="mt-2">
+                                        Step by step instructions (with pics) on how to replace Transmission Fluid & Filter.
+                                    </Card.Text>
+                                    <Button action variant="primary">Show me how</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         );
     }
 }
