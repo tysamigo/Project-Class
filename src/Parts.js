95a1e2e6d20b5432f51211a251f03312942c6528
Tyler Ramage <tysamigo@gmail.com>
2019-12-15 21:51:33 -0500

    add list of parts and body content to parts.js

--- src/Parts.js
+++ src/Parts.js
@@ -1,15 +1,33 @@
 /* eslint-disable no-unused-expressions */
 import React from 'react';
-import { Table, Container } from 'react-bootstrap';
+import { Table, Container, ListGroup } from 'react-bootstrap';
 
 export default class Parts extends React.Component {
 
     render() {
         return (
-        <Container className="heading pb-5">
-            <h1 className="mt-5">Parts</h1>
-            <Table striped bordered hover>               
-            </Table>
+        <Container className="pb-5">
+            <h1 className="heading mt-5 mb-5">Parts</h1>
+            <ListGroup className="pb-5">
+                <ListGroup.Item action variant="light">Automatic Transmission</ListGroup.Item>
+                <ListGroup.Item action variant="light">Body</ListGroup.Item>
+                <ListGroup.Item action variant="light">Body Hardware</ListGroup.Item>
+                <ListGroup.Item action variant="light">Brakes</ListGroup.Item>
+                <ListGroup.Item action variant="light">Cooling System</ListGroup.Item>
+                <ListGroup.Item action variant="light">Electrical</ListGroup.Item>
+                <ListGroup.Item action variant="light">Emmission System</ListGroup.Item>
+                <ListGroup.Item action variant="light">Engine</ListGroup.Item>
+                <ListGroup.Item action variant="light">Exhaust System</ListGroup.Item>
+                <ListGroup.Item action variant="light">Front Drive Axle</ListGroup.Item>
+                <ListGroup.Item action variant="light">Front Suspension</ListGroup.Item>
+                <ListGroup.Item action variant="light">Fuel System</ListGroup.Item>
+                <ListGroup.Item action variant="light">HVAC</ListGroup.Item>
+                <ListGroup.Item action variant="light">Maintenance & Lubrication</ListGroup.Item>
+                <ListGroup.Item action variant="light">Rear Suspension</ListGroup.Item>
+                <ListGroup.Item action variant="light">Steering</ListGroup.Item>
+                <ListGroup.Item action variant="light">Transfer Case</ListGroup.Item>
+                <ListGroup.Item action variant="light">Universal & Rear Axle</ListGroup.Item>
+            </ListGroup>
         </Container>
         );
     }
