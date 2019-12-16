050682cc0d46419e976e88af2efaf97c9e11e20e
Tyler Ramage <tysamigo@gmail.com>
2019-12-15 20:42:22 -0500

    refactor code to be cleaner by using Row and Col instead of previous groupitems

--- src/Troubleshooting/Solutions/BrakeSolutions/BrakeGrindingSolution.js
+++ src/Troubleshooting/Solutions/BrakeSolutions/BrakeGrindingSolution.js
@@ -9,39 +9,38 @@ export default class BrakeGrindingSolution extends React.Component {
     render() {
         return (
             <div>
-                
-                    <Container>
-                        <Row>
-                            <Col>
-                                <Card>
-                                    <Card.Header as="h5">Front Brakes</Card.Header>
-                                    <Card.Img variant="top" src={brakes1} />
-                                    <Card.Body>
-                                        <Card.Title>Replacing Front Brake Pads</Card.Title>
-                                        <Card.Text className="mt-2">
-                                            Step by step instructions (with pics) on how to replace the Front Brake Pads.
+                <Container>
+                    <Row>
+                        <Col>
+                            <Card>
+                                <Card.Header as="h5">Front Brakes</Card.Header>
+                                <Card.Img variant="top" src={brakes1} />
+                                <Card.Body>
+                                    <Card.Title>Replacing Front Brake Pads</Card.Title>
+                                    <Card.Text className="mt-2">
+                                        Step by step instructions (with pics) on how to replace the Front Brake Pads.
                                         </Card.Text>
-                                        <Link to="/BrakeGrindingSolutionFrontBrakes">
-                                            <Button action variant="primary">Show me how</Button>
-                                        </Link>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                            <Col>
-                                <Card>
-                                    <Card.Header as="h5">Rear Brakes</Card.Header>
-                                    <Card.Img variant="top" src={brakes2} />
-                                    <Card.Body>
-                                        <Card.Title>Replacing Rear Brake Pads</Card.Title>
-                                        <Card.Text className="mt-2">
-                                            Step by step instructions (with pics) on how to replace the Rear Brake Pads.
-                                    </Card.Text>
+                                    <Link to="/BrakeGrindingSolutionFrontBrakes">
                                         <Button action variant="primary">Show me how</Button>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        </Row>
-                    </Container>                                    
+                                    </Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                                <Card.Header as="h5">Rear Brakes</Card.Header>
+                                <Card.Img variant="top" src={brakes2} />
+                                <Card.Body>
+                                    <Card.Title>Replacing Rear Brake Pads</Card.Title>
+                                    <Card.Text className="mt-2">
+                                        Step by step instructions (with pics) on how to replace the Rear Brake Pads.
+                                    </Card.Text>
+                                    <Button action variant="primary">Show me how</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
 
